588121397c2ffd29813996eb6c1afe46086f9887
Mario <marioharper182@gmail.com>
2015-01-18 13:06:36 -0700

    Assingment now incorporates part of the guessing game algorithm.

--- Homework1.py
+++ Homework1.py
@@ -1,10 +1,23 @@
 __author__ = 'mario'
 
-clst = [6, 9, 20]
+def chickennugget_main():
 
-dumplist = [clst[0]*x+clst[1]*y+clst[2]*z for x in range(0,40) for y in range(0, 30) for z in range(0, 15)]
-set = list(set(dumplist))
-keyvalues = [set[x] for x in range(1,(len(set)-1)) if set[x]+1 != set[x+1] and set[x]<100]
+    clst = [6, 9, 20]
 
-answer = [max(keyvalues)+1]
-print("The highest integer of chicken nuggets puchaseable is:", answer[0])
+    dumplist = [clst[0]*x+clst[1]*y+clst[2]*z for x in range(0,40) for y in range(0, 30) for z in range(0, 15)]
+    setlist = list(set(dumplist))
+    keyvalues = [setlist[x] for x in range(1,(len(setlist)-1)) if setlist[x]+1 != setlist[x+1] and setlist[x]<100]
+
+    answer = [max(keyvalues)+1]
+    print("The highest integer of chicken nuggets purchasable is:", answer[0])
+
+def guessinggame_main():
+    import random
+
+    usernum = random.randint(1,100)
+    print("To save you the hassle, you choose to pick the number:", usernum)
+    print("The computer will now guess this number.")
+
+    guess = 50
+    while guess != usernum:
+        if guess > usernum:
