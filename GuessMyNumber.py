c167ef1c57050a74e1bc5ec68f7a414bd4c9220b
Mario <marioharper182@gmail.com>
2015-01-28 19:11:43 -0700

    Made some minor changes with the graphing support.

--- GuessMyNumber.py
+++ GuessMyNumber.py
@@ -1,18 +1,24 @@
 __author__ = 'Mario'
 
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+
 def Guessengine(A):
     print(A)
-    A = int(A)
+    numbertoguess = int(A)
 
     low = 0
     high = 101
 
+    matplotlist = [50]
+    trieslist = [0]
     guess = [50]
     g = 0
     counter = 0
     # i = 50
-    while guess[-1] != A:
-        if guess[-1] < A:
+    while guess[-1] != numbertoguess:
+        if guess[-1] < numbertoguess:
             if high is not None:
                 g = (high - guess[-1]) // 2 + guess[-1]
             else:
@@ -24,7 +30,7 @@ def Guessengine(A):
             if g == guess[-1]:
                 g = g + 1
 
-        if guess[-1] > A:
+        if guess[-1] > numbertoguess:
             if low is not None:
                 g = guess[-1] - (guess[-1] - low)// 2
             else: g = guess[-1] - (guess[-1] - min(guess)) // 2
@@ -38,9 +44,23 @@ def Guessengine(A):
         if counter > 1:
             guess.pop(0)
 
+        matplotlist.append(g)
         guess.append(g)
         counter = counter +1
+        trieslist.append(counter)
         print(guess[-1])
 
-    if guess[-1] == A:
+    if guess[-1] == numbertoguess:
         print("Success! Took", counter+1, "tries")
+
+        tuples = zip(trieslist, matplotlist)
+        plt.figure(1)
+        plt.plot(trieslist, matplotlist, 'bo',linestyle = '-', linewidth = 2.00)
+        # plt.plot([guess[-1], 0 ], [guess[-1], len(trieslist)], 'r', linestyle = '-', linewidth = 1.00)
+        # plt.plot(*zip(*itertools.chain.from_iterable(itertools.combinations(tuples, 2))), color = 'blue',marker = 'o', linewidth = 2.00)
+        plt.show()
+
+def Matplotwindow():
+    pass
+
+    # tries = trieslist
